018387d0e83bfa449070aaff98d7e1855c05525b
Pavel Belyaev <pashawh@gmail.com>
2020-10-28 21:36:43 +0300

    Change for .. of loop to a regular for (es5)

--- 5_stringExpansion.js
+++ 5_stringExpansion.js
@@ -3,9 +3,10 @@ function stringExpansion(str) {
    var newStr = []
    var num = 0
    var sym = ''
-   for (var char of str) {
-      isNaN(Number(char)) ? arr.push(char) : arr.push(+ char)      
-   }   
+   for (var index = 0; index < str.length; index++) {
+      var char = str[index]
+      isNaN(Number(char)) ? arr.push(char) : arr.push(+char)
+   }
    arr.map(function(item) {  
       if (typeof item === 'string') {
          sym = item
